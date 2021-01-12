12d23fcf7cb30daf7dbdba183f459e3fb4c42d2b
yassinerh28 <yassineelrhazouani28@gmail.com>
2021-01-12 20:04:28 +0100

    new sections

--- scroll.js
+++ scroll.js
@@ -1,7 +1,8 @@
 var navigation_bar = document.getElementById("navigation-bar");
 
+
 function scrollfunction() {
-    if (window.pageYOffset <= 657) {
+    if (window.pageYOffset == 0) {
         navigation_bar.classList.remove("navigation-bar-scroll");
     } else {
         navigation_bar.classList.add("navigation-bar-scroll");
