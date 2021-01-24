051e69df1e2108d79e1b20da977f545b320fd23c
yassinerh28 <yassineelrhazouani28@gmail.com>
2021-01-25 00:58:03 +0100

    right & left button functionality

--- scripts/update.js
+++ scripts/update.js
@@ -6,10 +6,46 @@ var button_right = document.getElementById("button-right");
 var progress_bar = document.getElementById("progress");
 
 var current_app_page = 1;
-var next_page = current_app_page + 1;
-var previous_page = current_app_page - 1;
+var pending_pages = [];
+var visited_pages = [1];
+var next_page = 1;
+var previous_page = 0;
+
+var selected_choices_values = [];
+
+function array_min(array) {
+    var array_min_return = array[0];
+    if (array.length > 1) {
+        for (let index = 1; index < array.length; index++) {
+            array_min_return = (array[index] - array_min_return < 0) ? array[index] : array_min_return;
+        }
+    }
+    return (array_min_return != undefined) ? array_min_return : 19;
+}
+
+function array_max(array) {
+    var array_min_return = array[0];
+    if (array.length > 1) {
+        for (let index = 1; index < array.length; index++) {
+            array_min_return = (array[index] - array_min_return > 0) ? array[index] : array_min_return;
+        }
+    }
+    return array_min_return;
+}
+
+function array_remove_value(array, value) {
+    for (let index = 0; index < array.length; index++) {
+        const choice = array[index];
+        if (choice == value) {
+            const index = array.indexOf(choice);
+            array.splice(index, 1);
+        }
+    }
+}
 
 function go_to_page(number) {
+    console.log("----------------");
+    console.log("start--going_to_page_" + number + "...");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -34,17 +70,102 @@ function go_to_page(number) {
             button_right.classList.add(app_page.dataset.button_right_type);
             progress_bar.style.width = app_page.dataset.progress + "%";
             current_app_page = number;
-            next_page = current_app_page + 1;
-            previous_page = current_app_page - 1;
+            previous_page = array_max(visited_pages.slice(0, -1));
+            add_pending_pages();
+            load_local_data(number);
+            console.log("log--pending_pages: " + pending_pages);
+            console.log("log--next_page: " + next_page);
+            console.log("log--visited_pages: " + visited_pages);
+            console.log("log--previous_page: " + previous_page);
+            console.log("end--going_to_page_" + number + "...");
         }
     };
     xhttp.open("GET", "app-" + number + ".html", true);
     xhttp.send();
 }
 
-var selected_choices_values = [];
+function load_local_data(number) {
+    console.log("start--loading_local_data...");
+    var current_app = document.getElementById("app-" + number);
+    if (localStorage.getItem("app-" + number)) {
+        var localStorage_item = localStorage.getItem("app-" + number);
+        var choices = document.getElementsByClassName('choice');
+        var input = document.getElementsByClassName("input")[0];
+        if (current_app.classList.contains("radio-choices")) {
+            for (let index = 0; index < choices.length; index++) {
+                const choice = choices[index];
+                if (index + 1 == localStorage_item) {
+                    update_app(choice);
+                }
+            }
+        } else if (current_app.classList.contains("multiple-choices")) {
+            console.log(localStorage_item);
+            for (let index = 0; index < choices.length; index++) {
+                const choice = choices[index];
+                for (let index2 = 0; index2 < localStorage_item.length; index2++) {
+                    const element = localStorage_item[index2];
+                    if (index + 1 == element) {
+                        update_app(choice);
+                        break;
+                    }
+                }
+            }
+        } else if (current_app.classList.contains("input-choice")) {
+            input.value = localStorage_item;
+        }
+    }
+    add_pending_pages();
+    console.log("log--pending_pages: " + pending_pages);
+    console.log("log--next_page: " + next_page);
+    console.log("end--loading_local_data...");
+}
+
+
+function add_pending_pages() {
+    console.log("start--adding_pending_pages...");
+    var current_app = document.getElementById("app-" + current_app_page);
+    var choices = document.getElementsByClassName('choice');
+    if (current_app.dataset.evident_next_page != "" && !pending_pages.includes(current_app.dataset.evident_next_page)) {pending_pages.push(current_app.dataset.evident_next_page);}
+    if (current_app.classList.contains("radio-choices") || current_app.classList.contains("multiple-choices")) {
+        for (let index = 0; index < choices.length; index++) {
+            const choice = choices[index];
+            array_remove_value(pending_pages, choice.dataset.go_to);
+            console.log("--removing_" + choice.dataset.go_to);
+        }
+        for (let index = 0; index < choices.length; index++) {
+            const choice = choices[index];
+            if (choice.dataset.go_to != "" && choice.classList.contains("choice-selected") && !pending_pages.includes(choice.dataset.go_to)) {
+                pending_pages.push(choice.dataset.go_to);
+                console.log("--adding_" + choice.dataset.go_to);
+            }
+        }
+    }
+    next_page = array_min(pending_pages);
+    console.log("log--pending_pages: " + pending_pages);
+    console.log("log--next_page: " + next_page);
+    console.log("end--adding_pending_pages...");
+}
+
+function remove_pending_pages() {
+    console.log("start--removing_pending_pages...");
+    var current_app = document.getElementById("app-" + current_app_page);
+    var choices = document.getElementsByClassName('choice');
+    array_remove_value(pending_pages, current_app.dataset.evident_next_page);
+    console.log("--removing_" + pending_pages, current_app.dataset.evident_next_page);
+    for (let index = 0; index < choices.length; index++) {
+        const choice = choices[index];
+        array_remove_value(pending_pages, choice.dataset.go_to);
+        console.log("--removing_" + choice.dataset.go_to);
+    }
+    next_page = array_min(pending_pages);
+    console.log("log--pending_pages: " + pending_pages);
+    console.log("log--next_page: " + next_page);
+    console.log("end--removing_pending_pages...");
+}
 
 function update_app(e) {
+    console.log("----------------");
+    console.log("start--update...");
     if (e.classList.contains("radio-choice")) {
         var choices = document.getElementsByClassName("choice");
         for (let index = 0; index < choices.length; index++) {
@@ -61,30 +182,34 @@ function update_app(e) {
         var choices = document.getElementsByClassName("choice");
         e.classList.toggle("choice-selected");
         if (e.classList.contains("choice-selected")) {
-            selected_choices_values.push(e.dataset.order);
+            if (!selected_choices_values.includes(e.dataset.order)) {selected_choices_values.push(e.dataset.order);}
         } else {
-            for (let index = 0; index < selected_choices_values.length; index++) {
-                const choice = selected_choices_values[index];
-                if (choice == e.dataset.order) {
-                    const index = selected_choices_values.indexOf(choice);
-                    selected_choices_values.splice(index, 1);
-                }
-            }
+            array_remove_value(selected_choices_values, e.dataset.order);
         }
         localStorage.setItem("app-" + current_app_page, selected_choices_values);
     } else if (e.classList.contains("input")) {
         localStorage.setItem("app-" + current_app_page, e.value);
     }
+    add_pending_pages();
+    console.log("log--pending_pages: " + pending_pages);
+    console.log("log--next_page: " + next_page);
+    console.log("end--loading_local_data...");
 }
 
-function left_button_action(e) {
+function left_button_action() {
+    console.log("----------------");
     go_to_page(previous_page);
+    array_remove_value(visited_pages, array_max(visited_pages));
+    remove_pending_pages();
     selected_choices_values = [];
 }
 
-function right_button_action(e) {
-    go_to_page(next_page);
+function right_button_action() {
+    console.log("----------------");
+    go_to_page(array_min(pending_pages));
+    array_remove_value(pending_pages, array_min(pending_pages));
+    visited_pages.push(next_page);
     selected_choices_values = [];
 }
 
-go_to_page(1);
+go_to_page(next_page);
