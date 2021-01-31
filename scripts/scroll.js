79bc9e42a0910ec0913255ad78193b2a608aa288
anaselbtioui73 <anaselbtioui73@gmail.com>
2021-01-31 20:37:34 +0100

    home page update

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,6 @@
 var navigation_bar = document.getElementById("navigation-bar");
 var pattern = document.getElementById("pattern");
+var values_intro_circle = document.getElementById("values-intro-circle");
 
 function scrollfunction() {
     if (window.pageYOffset == 0) {
@@ -8,4 +9,5 @@ function scrollfunction() {
         navigation_bar.classList.add("navigation-bar-scroll");
     }
     if (pattern) {pattern.style.transform = "translateY(-"+(window.pageYOffset/4)+"px)";}
+    if (values_intro_circle) {values_intro_circle.style.transform = "rotateZ("+(window.pageYOffset/16)+"deg)";}
 }
