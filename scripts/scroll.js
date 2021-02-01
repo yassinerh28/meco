8af3f4178ad681bfae443dbef8e40175e840e7fe
anaselbtioui73 <anaselbtioui73@gmail.com>
2021-02-01 07:11:06 +0100

    faq 830 breakpoint

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,13 +1,17 @@
 var navigation_bar = document.getElementById("navigation-bar");
 var pattern = document.getElementById("pattern");
 var values_intro_circle = document.getElementById("values-intro-circle");
+var page_heading = document.getElementById("page-heading");
+var cta_nav_link = document.getElementById("cta-nav-link");
 
 function scrollfunction() {
     if (window.pageYOffset == 0) {
         navigation_bar.classList.remove("navigation-bar-scroll");
     } else {
         navigation_bar.classList.add("navigation-bar-scroll");
+        cta_nav_link.classList.add("cta-nav-link-after-scroll");
     }
     if (pattern) {pattern.style.transform = "translateY(-"+(window.pageYOffset/4)+"px)";}
-    if (values_intro_circle) {values_intro_circle.style.transform = "rotateZ("+(window.pageYOffset/16)+"deg)";}
+    if (values_intro_circle) {values_intro_circle.style.transform = "rotateZ("+(window.pageYOffset/4)+"deg)";}
+    if (page_heading) {page_heading.style.backgroundSize = (window.innerWidth/8 - 0.3*window.pageYOffset) + "px"}
 }
