d2032ea420d1e401962dcaad87106f692b4c19d7
yassinerh28 <yassineelrhazouani28@gmail.com>
2021-02-06 00:15:22 +0100

    json to html function

--- scripts/close-app.js
+++ scripts/close-app.js
@@ -5,3 +5,12 @@ function save_clear_localStorage() {
     collected_data.contact = JSON.parse(collected_data.contact);
     localStorage.clear();
 }
+
+function JSON_to_HTML(json_object) {
+    var html_return = "";
+    for (let index = 0; index < Object.keys(json_object).length; index++) {
+        const key = Object.keys(json_object)[index];
+        html_return += "<div id='field_" + key + "'>" + json_object[key] + "</div>"
+    }
+    return html_return;
+}
